4a0c00768861e192e3b2d1dbd2416f4fd80118ec
richie <zhaojunl@gmail.com>
2016-09-27 10:46:45 +0800

    lesson7,8

--- lesson7/unittes1t.py
+++ lesson7/unittes1t.py
@@ -1,15 +1,25 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
-import unittes1t
+import unittest
 import doctest1
 __author__ = 'frm.kpmg'
-
-class TestUM(unittes1t.TestCase):
+#https://my.oschina.net/lionets/blog/268704
+class TestUM(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_number_3_4(self):
         self.assertEqual(doctest1.func(3),4)
 
+class simple_test(unittest.TestCase):
+    def setUp(self):
+        self.foo=list(range(10))
+
+    def test_1st(self):
+        self.assertEqual(self.foo.pop(),9)
+
+
+    def test_2st(self):
+        self.assertEqual(self.foo.pop(),9)
 if __name__=='__main__':
-    unittes1t.main()
+    unittest.main()
---
+++ lesson8/networkx_.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = 'frm.kpmg'
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G=nx.random_graphs.barabasi_albert_graph(100,1)
+nx.draw(G)
+plt.savefig("ba.png")
+plt.show()
