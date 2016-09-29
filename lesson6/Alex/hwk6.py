2f0c1d8d4b9da4673c1a61dada188a9bc37084c1
alexyli1 <yunpengli2008@hotmail.com>
2016-09-29 19:25:37 +0800

    Merge branch 'master' of https://github.com/harryKPMG/kpmgfrm_python
    Conflicts:
    	lesson3/homework/fixedBond.csv

---
+++ lesson6/Alex/hwk6.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from scipy.stats import norm
+from math import log, sqrt, exp
+import scipy.optimize as opt
+
+__author__ = 'alexyli1'
+
+# r,t,k,s,pv
+r=0.05
+t=0.25
+k=2.50
+s=2.45
+pv=0.1133
+
+def call_option_pricer(sigma,s, k, t, r, pv):
+    d1 = (log(s/k) + (r + 0.5 * sigma *sigma) * t) / sigma / sqrt(t)
+    d2 = d1 - sigma * sqrt(t)
+    return s * norm.cdf(d1) - k * exp(-r*t) * norm.cdf(d2)-pv
+
+result = opt.fsolve(call_option_pricer,[1.0],(s,k,t,r,pv))
+a= (k,t,result)
+print a
+
+
