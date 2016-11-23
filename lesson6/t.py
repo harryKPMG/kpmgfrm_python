6e8d1311a750d819faedaa7a839a2be5857dea9b
richie <zhaojunl@gmail.com>
2016-11-23 21:01:35 +0800

    daf

---
+++ lesson6/t.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = 'frm.kpmg'
+
+import numpy as np
+
+n=np.array([1,2,3])
+print n
+
+n=np.linspace(1,10,num=25)
+print n
+
+print np.sin(n)
+
+# sympy
